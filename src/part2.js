2c22dff451a863c3d63fbd788a2213982a306d9e
Simon Højberg <r.hackr@gmail.com>
2014-12-08 22:38:12 +0100

    part 2

--- src/part2.js
+++ src/part2.js
@@ -1,14 +1,52 @@
-var Greeter = React.createClass({
+var App = React.createClass({
+
+  getInitialState: function () {
+    return {
+      name: ''
+    };
+  },
+
   render: function () {
     return (
       <div>
-        Hello World.
+        <NameCapture name={this.state.name} onChange={this.handleNameChange} />
+        <Greeter name={this.state.name} />
       </div>
     );
+  },
+
+  handleNameChange: function (name) {
+    this.setState({ name: name });
+  }
+
+});
+
+var NameCapture = React.createClass({
+  render: function () {
+    return (
+      <input type='text' value={this.props.name} onChange={this.handleChange}/>
+    )
+  },
+
+  handleChange: function (ev) {
+    this.props.onChange(ev.target.value);
   }
 });
 
+var Greeter = React.createClass({
+  render: function () {
+    var name = this.props.name;
+
+    return (
+      <div>
+        Hello {name}
+      </div>
+    );
+  },
+
+});
+
 React.render(
-  <Greeter />,
+  <App />,
   document.querySelector('body')
 );
